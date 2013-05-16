0471ee72957b6fff8f81014cbab718fa3e2ae0f0
silegon <silegon@gmail.com>
2013-05-16 12:53:38 +0800

    fix

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@
 # "zhoukh"<code@forpm.net> 2013-05-15 19:14:23
 import urllib2
 import threading
+import base64
 
 from models import MokerRequest, MokerResponse
 
@@ -25,15 +26,9 @@ class AsyncSendRequest(threading.Thread):
         self._async_send_request()
 
     def _async_send_request(self):
-        try:
-            response = urllib2.urlopen(self.req, None, TIME_OUT)
-        except urllib2.URLError:
-            response = None
+        response = urllib2.urlopen(self.req, None, TIME_OUT)
         moker_response = MokerResponse.objects.get(pk=self.moker_response_id)
-        if response:
-            moker_response.body = SEPERATOR.join([str(response.headers), response.read()])
-        else:
-            moker_response.body = 'ERROR'
+        moker_response.body = SEPERATOR.join([str(response.headers), response.read()])
         moker_response.save()
 
 def copy_request(request):
@@ -41,18 +36,19 @@ def copy_request(request):
     mu.uri = request.build_absolute_uri()
     first_line = request.method + ' ' + request.build_absolute_uri() + ' ' + request.META['SERVER_PROTOCOL'] + '\n'
     request_headers = "".join(["%s:%s\n" % (header[5:], value) for header, value in request.META.items() if header.startswith('HTTP_')])
-    mu.body = SEPERATOR.join(first_line, request_headers, request.body)
+    mu.body = SEPERATOR.join([first_line, request_headers, request.body])
     mu.save()
 
 def get_response(moker_request_id):
     moker_request = MokerRequest.objects.get(pk=moker_request_id)
     if moker_request.body:
-        first_line, request_headers, request_body = moker_request.body.split("$$$$$")
+        first_line, request_headers, request_body = moker_request.body.split(SEPERATOR)
         request_method, request_uri, request_protocal = first_line.split()
         request_headers_dict = {}
         for item in request_headers.split('\n'):
-            key, value = item.split(':')
-            request_headers_dict[key] = value.strip()
+            if item:
+                key, value = item.split(':', 1)
+                request_headers_dict[key] = value.strip()
     else:
         request_body = None
         request_headers_dict = {} 
@@ -64,3 +60,17 @@ def get_response(moker_request_id):
 
     req = urllib2.Request(moker_request.uri, request_body, request_headers_dict)
     async_send_request(req, moker_response.pk)
+
+def mock_remote_request(request):
+    _p = eval(base64.b64decode(request.POST['data']))
+    uri = _p['uri']
+    server_protocol = _p['server_protocol']
+    method = _p['method']
+    request_headers = _p['request_headers']
+    body = _p['body']
+
+    mu = MokerRequest()
+    mu.uri = uri
+    first_line = method + ' ' + uri + ' ' + server_protocol + '\n'
+    mu.body = SEPERATOR.join([first_line, request_headers, body])
+    mu.save()
--- views.py
+++ views.py
@@ -4,8 +4,17 @@
 
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from utils import mock_remote_request
 
 def send(request, template_name='moker/send.html'):
     context = {}
     return render_to_response(template_name, context,
                               context_instance=RequestContext(request))
+
+@csrf_exempt
+def mock_request(request):
+    mock_remote_request(request)
+    return HttpResponse('')
